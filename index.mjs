4ce3c0cddffce77e415346c6630faceea59e562a
BuildTools <unconfigured@null.spigotmc.org>
2023-02-23 15:09:45 +0100

    first commit

---
+++ index.mjs
@@ -0,0 +1,23 @@
+const args = process.argv.slice(2);
+
+
+
+
+import { Client, Server } from 'node-osc';
+
+const client = new Client('192.168.56.1', 3333);
+var server = new Server(3334, '0.0.0.0');
+
+server.on('listening', () => {
+  console.log('OSC Server is listening.');
+})
+
+server.on('message', (msg) => {
+  console.log(`Message: ${msg}`);
+  server.close();
+});
+
+client.send(args, (err) => {
+  if (err) console.error(err);
+  client.close();
+});
